94cce9165917807b6acbeeadfb9d7891fe34e6d2
rafidhaque <rafhaq4@gmail.com>
2022-03-18 17:44:27 +0600

    Limiting how many results

--- bs.py
+++ bs.py
@@ -5,6 +5,6 @@ import re
 with open('index.html', 'r') as f:
     doc = BeautifulSoup(f, 'html.parser')
 
-result = doc.find_all(text=re.compile("\$.*"))
+result = doc.find_all(text=re.compile("\$.*"), limit=1)
 for tag in result:
     print(tag.strip())
